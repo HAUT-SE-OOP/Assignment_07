e88fa19f63e65ea0ec7e5618496b868e9936614d
201816040126 <56550647+9990403@users.noreply.github.com>
2019-12-11 22:49:14 +0800

    Create SalariedEmployee.cpp

---
+++ 201816040126/Ex12_15/SalariedEmployee.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Employee.h"
+
+#include "SalariedEmployee.h"
+
+using namespace std;
+
+SalariedEmployee::SalariedEmployee( const string &first, const string &last,
+                         const string &ssn, double salary )
+            :Employee( first, last , ssn )
+            {
+                setWeeklySalary( salary );
+            }
+
+void SalariedEmployee::setWeeklySalary( double salary )
+{
+    if( salary >= 0)
+    WeeklySalary = salary;
+    else
+       throw invalid_argument("Weekly salary should be greater than 0!") ;
+}
+
+double SalariedEmployee::getWeeklySalary()
+{
+    return WeeklySalary;
+}
+
+void SalariedEmployee::earnings()
+{
+    cout << "The salaried worker's earning is: " << endl;
+    cout << getWeeklySalary() << endl;
+}
+//function to show earnings
