31ce55654d4a472342bf053189433463e89dcff1
201816040302 <56759921+201816040302@users.noreply.github.com>
2019-12-14 17:10:37 +0800

    Create BasePlusCommissionEmployee.h

---
+++ 201816040302/Ex12_15/BasePlusCommissionEmployee.h
@@ -0,0 +1,28 @@
+//BasePlusCommissionEmployee.h
+//BasePlusCommissionEmployee class derived from CommissionEmployee.
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+#include<string>
+#include"CommissionEmployee.h"
+
+using namespace std;
+
+class BasePlusCommissionEmployee : public CommissionEmployee
+{
+public:
+
+    BasePlusCommissionEmployee(const string &, const string &,
+                        const string &, double = 0.0, double = 0.0, double = 0.0);
+    virtual ~BasePlusCommissionEmployee() { }   //virtual destructor
+
+    void setBaseSalary(double); //set base salary
+    double getBaseSalary()const;    //return base salary
+
+    //keyword virtual signals intent to override
+    virtual double earnings() const override;   //calculate earnings
+    virtual void print() const override;    //print object
+private:
+    double baseSalary;  // vase salary per week
+};
+
+#endif // BASEPLUS_H
