aad53a1a5422ad42f5c77a1ede32b39b3d32f194
201816040316 <56966143+201816040316@users.noreply.github.com>
2019-12-15 18:06:32 +0800

    Create BasePlusCommissionEmployee.h

---
+++ 201816040316/Ex12_15/BasePlusCommissionEmployee.h
@@ -0,0 +1,21 @@
+#ifndef BASEPLUSCOMMISSIONEMPLOYEE_H_INCLUDED
+#define BASEPLUSCOMMISSIONEMPLOYEE_H_INCLUDED
+#include <string>
+#include "CommissionEmployee.h" 
+class BasePlusCommissionEmployee: public CommissionEmployee
+{
+public:
+    BasePlusCommissionEmployee( const std::string &，const std::string&, const std::string &, double = 0.0, double = 0,double = 0.0);
+virtual ~CommissionEmployee() { } /
+void setBaseSalary( double ); 
+double getBaseSalary() const; 
+virtual double earnings() const override; 
+virtual void print()const override;
+private:
+double baseSalary; 
+}; 
+#endif 
+
+
+
+#endif // BASEPLUSCOMMISSIONEMPLOYEE_H_INCLUDED
