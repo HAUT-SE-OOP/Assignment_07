4bb7fae6fe906184472d138cf2d878ec16b3837e
201816040230 <56499333+201816040230@users.noreply.github.com>
2019-12-15 12:01:43 +0800

    Create BasePlusCommissionEmployee.cpp

---
+++ 201816040230/Ex12_15/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdexcept>
+#include"BasePlusCommissionEmployee.h"
+using namespace std;
+
+ BasePlusCommissionEmployee::BasePlusCommissionEmployee(string first,string last,string ssn,double sales,double rate,double salary)
+ :CommissionEmployee(first,last,ssn,sales,rate)
+ {
+     setBaseSalary(salary);
+ }
+void BasePlusCommissionEmployee::setBaseSalary(double salary)
+{
+    if(salary>=0.0)
+        baseSalary=salary;
+    else
+        throw invalid_argument("Salary must be >=0.0");
+}
+double BasePlusCommissionEmployee::getBaseSalary()
+{
+    return baseSalary;
+}
+double BasePlusCommissionEmployee::earnings()
+{
+    return getBaseSalary()+CommissionEmployee::earnings();
+}
+ void BasePlusCommissionEmployee::print()
+{
+    cout<<"base-salaried";
+    CommissionEmployee::print();
+    cout<<";base salary:"<<getBaseSalary();
+
+}
