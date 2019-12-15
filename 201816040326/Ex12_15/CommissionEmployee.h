ea9bb8aeeb9d1f8f2cf79ac8b6c97aed058f1eb8
zyyytt <54845294+zyyytt@users.noreply.github.com>
2019-12-15 16:53:54 +0800

    Create CommissionEmployee.h

---
+++ 201816040326/Ex12_15/CommissionEmployee.h
@@ -0,0 +1,29 @@
+#ifndef COMMISSIONEMPLOYEE_H_INCLUDED
+#define COMMISSIONEMPLOYEE_H_INCLUDED
+#include "Employee.h"
+#include <string>
+using namespace std;
+
+class CommissionEmployee : public Employee
+{
+public:
+    CommissionEmployee( const string &, const string & , const string & ,double=0.0, double=0.0);
+    virtual ~CommissionEmployee() {}//virtual destructor
+
+    void setCommissionRate( double );
+    double getCommissionRate() const;
+
+    void setGrossSales( double );
+    double getGrossSales() const;
+
+    virtual double earnings() const override;
+    virtual void print() const override;
+
+private:
+    double grossSales;
+    double commissionRate;
+
+};
+
+
+#endif // COMMISSIONEMPLOYEE_H_INCLUDED
