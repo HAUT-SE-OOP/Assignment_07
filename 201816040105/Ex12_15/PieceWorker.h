706ac826446d96f08898bcefdd30501d65609c1d
David-0424 <56601563+David-0424@users.noreply.github.com>
2019-12-15 18:44:24 +0800

    Create PieceWorker.h

---
+++ 201816040105/Ex12_15/PieceWorker.h
@@ -0,0 +1,26 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+#include <string>
+#include "Employee.h"
+
+class PieceWorker : public Employee
+{
+	public:
+		PieceWorker(const string &, const string &, const string &, double=0.0, double=0.0);
+		virtual ~PieceWorker(){}
+		
+		void setWage(double);
+		double getWage() const;
+		
+		void setPieces(double);
+		double getPieces() const;
+		
+		virtual double earnings() const override;
+		virtual void print() const override;
+	private:
+		double wage;
+		double pieces;
+};
+
+#endif
