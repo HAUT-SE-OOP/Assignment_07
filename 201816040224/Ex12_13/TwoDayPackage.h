faf3978b22c5e837851630651b3b57b019ead843
Ricardo-hs <56646943+Ricardo-hs@users.noreply.github.com>
2019-12-15 13:33:28 +0800

    Create TwoDayPackage.h

---
+++ 201816040224/Ex12_13/TwoDayPackage.h
@@ -0,0 +1,19 @@
+#ifndef TWODAYPACKAGE_H_INCLUDED
+#define TWODAYPACKAGE_H_INCLUDED
+
+#include "Package.h"
+
+class TwoDayPackage : public Package
+{
+    public:
+    TwoDayPackage(const string &,const string &,const string &,
+    const string &,const string &,const string &,
+    double = 0.0,double = 0.0,double = 0.0);
+    void setTwoDayPrice(double twodayprice);
+    void print();
+    private:
+        double twoDayPrice;
+        double totalPrice;
+};
+
+#endif // TWODAYPACKAGE_H_INCLUDED
