ed3a9e871b95ffa1598977db078ab09cf55cdf02
zifeiyu-c <56395947+zifeiyu-c@users.noreply.github.com>
2019-12-15 11:55:57 +0800

    Create BasePlusCommissionEmployee.cpp

---
+++ 201816040112/Ex12_15.cpp/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdexcept>
+#include "BasePlusCommissionEmployee.h"
+using namespace std;
+//constructor
+BasePlusCommissionEmployee::BasePlusCommissionEmployee( const string &first , const string &last , const string &ssn , double sales , double rate , double salary )
+:CommissionEmployee( first , last , ssn , sales , rate )
+{
+    setBaseSalary( salary );//validate and store base salary
+}
+// set base salary
+void BasePlusCommissionEmployee::setBaseSalary( double salary )
+{
+    if ( salary >= 0.0 )
+        baseSalary = salary;
+    else
+        throw invalid_argument( "Salary must be >= 0.0 " );
+}//end function setBaseSalary
+//return base salary
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+}//end function setBaseSalary
+//return base salary
+double BasePlusCommissionEmployee::earnings()
+{
+    return getBaseSalary() + CommissionEmployee::earnings();
+}//end function earnings
+
+void BasePlusCommissionEmployee::print()
+{
+    cout << "base-salaried ";
+    CommissionEmployee::print();
+    cout << "; base salary : " << getBaseSalary() ;
+}
