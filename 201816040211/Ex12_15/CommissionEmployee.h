749d124fd2a4a6fec9c62466e4db47a048e568fc
201816040211 <56756575+201816040211@users.noreply.github.com>
2019-12-15 16:36:50 +0800

    Add files via upload

---
+++ 201816040211/Ex12_15/CommissionEmployee.h
@@ -0,0 +1,29 @@
+#ifndef COMMISSIONEMPLOYEE_H_INCLUDED
+#define COMMISSIONEMPLOYEE_H_INCLUDED
+
+#include <string>//C++ standard string class
+#include "Employee.h"
+using namespace std;
+
+class CommissionEmployee: public Employee
+{
+public:
+    CommissionEmployee( const string &, const string &, const string &,
+                       double = 0.0, double = 0.0 );
+    virtual ~CommissionEmployee() {} // virtual destructor
+
+    void setCommissionRate( double ); //set commission rate
+    double getCommissionRate() const; //return commission rate
+
+    void setGrossSales( double ); //set gross sales amount
+    double getGrossSales() const; //return gross sales amount
+
+    //keyword virtual signals intent to override
+    virtual double earnings() const override;//calculate earnings
+    virtual void print() const override; //print object
+private:
+    double grossSales; //gross weekly sales
+    double commissionRate; //commission percentage
+}; // end class CommissionEmployee
+
+#endif // COMMISSIONEMPLOYEE_H_INCLUDED
