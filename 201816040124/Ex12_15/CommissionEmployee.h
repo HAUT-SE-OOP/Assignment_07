7712206a38acc64458dad814e2db53b086f08117
201816040124陈帆 <56501972+201816040124@users.noreply.github.com>
2019-12-15 18:08:05 +0800

    Create CommissionEmployee.h

---
+++ 201816040124/Ex12_15/CommissionEmployee.h
@@ -0,0 +1,28 @@
+#ifndef COMMISSION_H
+#define COMMISSION_H
+
+#include <string> // C++ standard string class
+#include"Employee.h"
+using namespace std;
+
+class CommissionEmployee:public Employee
+{
+public:
+   CommissionEmployee( const string &, const string &, const string &,
+      double = 0.0, double = 0.0 );
+   virtual ~CommissionEmployee(){}
+
+   void setGrossSales( double ); // set gross sales amount
+   double getGrossSales() const; // return gross sales amount
+
+   void setCommissionRate( double ); // set commission rate (percentage)
+   double getCommissionRate() const; // return commission rate
+
+   virtual double earnings() const override; // calculate earnings
+   virtual void print() const override; // print CommissionEmployee object
+private:
+   double grossSales; // gross weekly sales
+   double commissionRate; // commission percentage
+}; // end class CommissionEmployee
+
+#endif
