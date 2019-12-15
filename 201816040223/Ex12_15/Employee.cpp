b5e713725a2a0ce7faf59a15169a36ab538b8941
201816040223 <56751327+201816040223@users.noreply.github.com>
2019-12-15 17:33:50 +0800

    Create Employee.cpp
    This is my assignment;
    My student ID:201816040223;

---
+++ 201816040223/Ex12_15/Employee.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include "Employee.h"
+using namespace std;
+//constructor
+Employee::Employee(const string &first,const string &last,const string &ssn)
+{
+    setFirstName(first);
+    setLastName(last);
+    setSocialSecurityNumber(ssn);
+}//end Employee constructor
+//set first name
+void Employee::setFirstName(const string &first)
+{
+    firstName=first;
+}//end function setFirstName
+string Employee::getFirstName() const
+{
+    return firstName;
+}//end function getFirstName
+//set last name
+void Employee::setLastName(const string &last)
+{
+    lastName=last;
+}//end function setFirstName
+string Employee::getLastName() const
+{
+    return lastName;
+}//end function getLastName
+//set SSN
+void Employee::setSocialSecurityNumber(const string &ssn)
+{
+    socialSecurityNumber=ssn;
+}//end function setSocialSecurityNumber
+string Employee::getSocialSecurityNumber() const
+{
+    return socialSecurityNumber;
+}//end function getSocialSecurityNumber
+void Employee::print()const
+{
+    cout<<"first name : "<<getFirstName()<<endl;
+    cout<<"last name : "<<getLastName()<<"socialSecurityNumber : "<<getSocialSecurityNumber()<<endl;
+
+}//end function print
