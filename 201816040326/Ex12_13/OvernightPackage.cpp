95a81ef4567fa1d9151ef3d9b2f51b5d18af4c49
zyyytt <54845294+zyyytt@users.noreply.github.com>
2019-12-15 16:15:29 +0800

    Create OvernightPackage.cpp

---
+++ 201816040326/Ex12_13/OvernightPackage.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<string>
+#include"Package.h"
+#include"TwoDayPackage.h"
+#include"OvernightPackage.h"
+using namespace std;
+
+OvernightPackage::OvernightPackage(string sname,string rname,string add,string c,string s,string code,double weight,double cost,double ofee)
+:Package(sname,rname,add,c,s,code,weight,cost)//初始化数据成员
+{
+    if(ofee > 0)
+    {
+        OvernightFee = ofee;
+    }
+    else
+    {
+        OvernightFee = 0;
+    }
+}
+
+double OvernightPackage::calculate_Cost()//将隔夜额外服务费加入标准费用中计算新费用
+{
+    return getweight()*( getcost() + OvernightFee );
+}
+
+double OvernightPackage::getOvernightFee()
+{
+    return OvernightFee;
+}
+
+void OvernightPackage::print()
+{
+    Package::print();
+    cout<<"OvernightFee: "<<getOvernightFee()<<endl;
+}
