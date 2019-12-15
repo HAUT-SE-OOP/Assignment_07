0ea6f391672da4600a9656633170051c13e4cd92
201816040103 <56601097+201816040103@users.noreply.github.com>
2019-12-15 12:54:07 +0800

    Create Package.cpp

---
+++ 201816040103/Ex12_13/Package.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <string>
+#include<iomanip>
+#include<bits/stdc++.h>
+#include "Package.h"
+using namespace std;
+
+Package::Package(const string &nam,const string &nams,const string &add,const string &cit,const string &sta,
+		const string& pos,double wei,double fe)
+{
+	setName(nam,nams);
+	setAddress(add);
+	setCity(cit);
+	setState(sta);
+	setPostcode(pos);
+	setWeight(wei);
+	setFee(fe);
+}
+
+void Package::setName(string nam,string nams)
+{
+	name1 = nam;
+	name2 = nams;
+} 
+
+string Package::getName()
+{
+	return name1,name2;
+}
+
+void Package::setAddress(string add)
+{
+	address = add;
+}
+
+string Package::getAddress()
+{
+	return address;
+}
+
+void Package::setCity(string cit)
+{
+	city = cit;
+}
+
+string Package::getCity()
+{
+	return city;
+}
+
+void Package::setState(string sta)
+{
+	state = sta;
+}
+
+string Package::getState()
+{
+	return state;
+}
+
+void Package::setPostcode(string pos)
+{
+	postcode = pos;
+}
+
+string Package::getPostcode()
+{
+	return postcode;
+}
+
+void Package::setWeight(double wei)
+{
+	if(wei > 0)
+		weight = wei;
+	else
+		throw invalid_argument("weight must be greater than 0");
+}
+
+double Package::getWeight()
+{
+	return weight;
+}
+
+void Package::setFee(double fe)
+{
+	if(fe > 0)
+		fee = fe;
+	else
+		throw invalid_argument("fee must be greater than 0");	
+}
+
+double Package::getFee()
+{
+	return fee;
+}
+
+double Package::calculateCost()
+{
+	return getWeight()*getFee();
+}
+
+void Package::print()
+{
+	cout<<getAddress()<<endl<<getCity()<<endl<<getState()<<endl;
+}
