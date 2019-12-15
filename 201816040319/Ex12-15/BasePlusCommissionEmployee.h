6d7fdb6039a8d517c7b69b453656b203d881ce8f
zhouyuxaing <56668463+zhouyuxaing@users.noreply.github.com>
2019-12-15 16:50:28 +0800

    Create BasePlusCommissionEmployee.h
    This is  my Assignment—07；
    student ID:201816040319

---
+++ 201816040319/Ex12-15/BasePlusCommissionEmployee.h
@@ -0,0 +1,23 @@
+//assignment12-15
+//base.h
+
+#ifndef BASEPLUS_H
+#define BASEPLUS_H
+#include <iostream>
+#include <string>
+#include "CommissionEmployee.h"
+using namespace std;
+
+class BasePlusCommissionEmployee:public CommissionEmployee  //definition of class BaseP
+{
+public:
+    BasePlusCommissionEmployee(const string &,const string &,const string &,double,double,double); //constructor of BasePlusCommissionEmployee
+    void setBaseSalary(double); //declare function to set BaseSalary
+    double getBaseSalary()const;
+
+    virtual double earnings()const override; //declare function to earnings
+    virtual void print() const override;
+private:
+    double baseSalary; //data menber baseSalary
+};
+#endif // BASEPLUS_H
