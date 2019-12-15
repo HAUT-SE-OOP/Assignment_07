6f9a81f81472c4a482181745db4b2d9bfd830528
yuebeifan <48322819+yuebeifan@users.noreply.github.com>
2019-12-15 16:08:36 +0800

    Add files via upload

---
+++ 201816040108/Ex12_15/BasePlusCommissionEmployee.cpp
@@ -0,0 +1,44 @@
+
+//BasePlusCommissionEmployee.cpp
+//BasePlusCommissionEmployee member-function definitions.
+#include<iostream>
+#include<stdexcept>
+#include"BasePlusCommissionEmployee.h"
+using namespace std;
+
+//constructor
+BasePlusCommissionEmployee::BasePlusCommissionEmployee(const string &first, const string &last, const string &ssn,
+                                double sales,double rate,double salary)
+                                :CommissionEmployee(first,last,ssn,sales,rate)
+{
+    setBaseSalary(salary);  //validate and store base salary
+}
+//set base salary
+void BasePlusCommissionEmployee::setBaseSalary(double salary)
+{
+    if(salary>=0.0)
+        baseSalary=salary;
+    else
+        throw invalid_argument("Salary must be >= 0.0");
+}
+
+//return base salary
+double BasePlusCommissionEmployee::getBaseSalary() const
+{
+    return baseSalary;
+}
+
+//calculate earnings;
+//override virtual function earnings in CommissionEmployee
+double BasePlusCommissionEmployee::earnings() const
+{
+    return getBaseSalary()+CommissionEmployee::earnings();
+}
+
+//print BasePlusCommissionEmployee's information
+void BasePlusCommissionEmployee::print() const
+{
+    cout << "base-salaried ";
+    CommissionEmployee::print();    //code reuse
+    cout<< " base salary: " << getBaseSalary() << endl;
+}
---
+++ 201816040108/Ex12_15/Ex12_15.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<iomanip>
+#include<vector>
+#include"Employee.h"
+#include"SalariedEmployee.h"
+#include"CommissionEmployee.h"
+#include"BasePlusCommissionEmployee.h"
+#include"PieceWorker.h"
+#include"HourlyEmployee.h"
+using namespace std;
+int main()
+{
+    //set floating-point output formatting
+    cout << fixed << setprecision(2);
+
+    //create derived-class objects
+    SalariedEmployee salariedEmployee("Wang","Jun","111-11-1111",800);
+    CommissionEmployee commissionEmployee("Jiang","Feng","222-22-2222",10000,.06);
+    BasePlusCommissionEmployee basePlusCommissionEmployee("Tom","Simth","333-33-3333",5000,.04,300);
+    PieceWorker pieceWorker("LI","YIfeng","444-44-4444",20,5);
+    HourlyEmployee hourlyEmployee("Hu","GE","555-55-5555",10,50);
+
+    //create vector of five base-class pointer
+    vector < Employee * > EPtr(5);
+
+    //initialize vector with pointers to Employees
+    EPtr[0]= &salariedEmployee;
+    EPtr[1]= &commissionEmployee;
+    EPtr[2]= &basePlusCommissionEmployee;
+    EPtr[3]= &pieceWorker;
+    EPtr[4]= &hourlyEmployee;
+
+for( size_t i=0; i<EPtr.size(); i++)
+
+    {
+        EPtr[i]->print();
+        cout<<endl;
+
+    }
+
+}
+
