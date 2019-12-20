9a36b094d05a6154d7d6845291fe269f782abac9
17744602018 <56758178+17744602018@users.noreply.github.com>
2019-12-20 18:14:10 +0800

    Create TwoDayPackage.cpp

---
+++ 201816040307/Ex12_13/TwoDayPackage.cpp
@@ -0,0 +1,20 @@
+
+#include "TwoDayPackage.h"
+
+TwoDayPackage::TwoDayPackage(double a, const string name, const string address, const string city, const string state,
+    const string zip, double weight, double costper)
+    :Package(name, address, city, state, zip, weight, costper)
+{
+    flatFee = a;
+}
+
+double TwoDayPackage::calculateCost()
+{
+    return Package::calculateCost() + flatFee;
+}
+
+string TwoDayPackage::getAddress()
+{
+    string str =  getCity() + " " + getState();
+    return str;
+}
